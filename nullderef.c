b952ce7d66ca070d9e7c2721e963550053dc96ab
Ben Smith <le.ben.smith@gmail.com>
2022-04-12 17:47:55 -0700

    program that derefs a null pointer

---
+++ nullderef.c
@@ -0,0 +1,11 @@
+//#include <stdio.h>
+#include "types.h"
+#include "user.h"
+
+int main(int argc, char **argv)
+{
+  int *ptr = 0;
+
+  //printf(1, "Here it is: %d\n", *ptr);
+  int test = *ptr;
+}
